35bdb7c496be67dcbbbc3a9e038bd96ff0ba8b10
Aniket281004 <aniketsharma281004@gmail.com>
2025-08-14 20:59:46 +0530

    Update EDA.py

--- Model/EDA.py
+++ Model/EDA.py
@@ -14,3 +14,14 @@ df['Age'].sort_values(ascending=False).head(10)
 for i in df.columns:
     if i!='Age':
         print(i,df[i].unique())
+features = ['Age','Gender', 'self_employed', 'family_history', 'work_interfere', 'remote_work', 'benefits', 'care_options', 'wellness_program', 'seek_help', 'leave', 'mental_health_consequence', 'coworkers', 'supervisor', 'mental_health_interview' ]
+columns_to_plot = features
+for col in columns_to_plot:
+    ct = pd.crosstab(df[col],df['treatment'])
+    ct.plot(kind='bar',stacked =True)
+    plt.title(f'Treatment vs {col}')
+    plt.xlabel(f'{col}')
+    plt.ylabel('Count')
+    plt.legend(title="Treatment")
+    plt.tight_layout()
+    plt.show()
