ae8a3e0d3f39a815b768ad84c572460fba0ae248
Aniket281004 <aniketsharma281004@gmail.com>
2025-08-15 10:52:55 +0530

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -299,7 +299,7 @@ elif page == 'Clustering':
     for description, img_path in cluster_images.items():
         try:
             image = Image.open(img_path)
-            st.image(image, caption=description, width=200)
+            st.image(image, caption=description, width=400)
         except FileNotFoundError:
             st.warning(f"Image not found: {img_path}")
 
